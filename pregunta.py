63185e58adc116d454a7c5a5bc96e10a6888f61c
Carlos Cuastumal <carloscuastumal64@gmail.com>
2023-03-21 21:06:50 -0500

    se realiza correcion de punto 4

--- pregunta.py
+++ pregunta.py
@@ -19,8 +19,7 @@ def ingest_data():
     df=df.dropna()
     df=df.reset_index()
     del df["index"]
-    df=df.replace(r'ªs+', ' ', regex=True)
-    df['principales_palabras_clave']= df['principales_palabras_clave'].str.replace('.','')
+    df['principales_palabras_clave']= df['principales_palabras_clave'].str.replace(r'\s+', ' ', regex=True)
     df['porcentaje_de_palabras_clave']= df['porcentaje_de_palabras_clave'].str[:-2]
     df['porcentaje_de_palabras_clave']= df['porcentaje_de_palabras_clave'].str.replace(',','.')
     df['porcentaje_de_palabras_clave']= pd.to_numeric(df['porcentaje_de_palabras_clave'])
